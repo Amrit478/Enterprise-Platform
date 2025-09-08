73ff96cd74e6d892c2fd26270439311532049b63
Amrit478 <Amritraj4786@gmail.com>
2025-09-07 21:23:43 -0400

    Initial commit: Add Spring Boot Enterprise Platform project

---
+++ src/main/java/com/example/enterprise/SpringBootEnterprisePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEnterprisePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEnterprisePlatformApplication.class, args);
+	}
+
+}
